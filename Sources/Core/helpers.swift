11ea93fdad55b10f48af1cd08894ee8dee61902d
Frédéric Ruaudel <github@fredericruaudel.fr>
2024-12-02 17:20:08 +0100

    Refactor Advent of Code 2024 execution logic
    - Moved input handling and execution logic into a new `AoC24` enum in `helpers.swift`.
    - Updated `main.swift` to utilize the new `AoC24.run` method for executing the Day 1 challenge.
    - Removed redundant input function from `main.swift` to streamline the code.

---
+++ Sources/Core/helpers.swift
@@ -0,0 +1,19 @@
+import Foundation
+
+public enum AoC24 {
+    private static let currentDirectory = FileManager.default.currentDirectoryPath
+
+    private static func input(forDay day: Int) throws -> String {
+        let filepath = "\(currentDirectory)/Inputs/day\(day).txt"
+        return try String(contentsOfFile: filepath, encoding: .utf8)
+    }
+
+    public static func run<T: AoCDay>(day: Int, using _: T.Type) throws {
+        let dayInput = try input(forDay: day)
+        let day = T()
+        print("Day \(day) answers")
+        print("-------------")
+        try print("Part1: <\(day.runPart1(with: dayInput))>")
+        try print("Part2: <\(day.runPart2(with: dayInput))>")
+    }
+}
